c57f0254ee117859bf0e77d003ae0a870e42730e
eyeck3 <43934315+eyeck3@users.noreply.github.com>
2020-02-16 14:19:13 -0600

    Step 4

--- app/src/main/java/com/example/lab3milestone2/MainActivity.java
+++ app/src/main/java/com/example/lab3milestone2/MainActivity.java
@@ -8,7 +8,7 @@ import android.app.FragmentManager;
 
 import android.app.FragmentTransaction;
 
-// import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 
 import android.view.View;
 
